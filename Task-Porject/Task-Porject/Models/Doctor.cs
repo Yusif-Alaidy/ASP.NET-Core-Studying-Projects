cd57c5d1d8e89f20e3a0602725fbc43fcf8b966f
Yusif Alaidy <yusifalaidy@gmail.com>
2025-08-10 17:44:37 +0300

    Fix: fix running bug

--- Task-Porject/Task-Porject/Models/Doctor.cs
+++
@@ -1,8 +0,0 @@
-﻿
-    public class Doctor
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Specialization { get; set; }
-        public string Img { get; set; }
-    }
---
+++ Task_project/Task_project/Controllers/DoctorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_project.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        public IActionResult BookAppointment()
+        {
+            return View();
+        }
+        public IActionResult CompleteAppointment()
+        {
+            return View();
+        }
+        public IActionResult ReservationsManagement()
+        {
+            return View();
+        }
+    }
+}
